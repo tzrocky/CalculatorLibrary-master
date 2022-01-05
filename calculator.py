25f0e27aafe79c893ece6a60ae5521c8b61c8c54
tzrocky <tzrocky@yahoo.fr>
2022-01-05 19:56:26 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 """Calculator library containing basic math operations."""
 
-@overload
+
 def add(first_term, second_term):
     return first_term + second_term
 
-@overload
+
 def subtract(first_term, second_term):
     return first_term - second_term
 
-@overload
+
 def multiply(first_term, second_term):
     return first_term * second_term
