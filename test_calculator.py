49076da405c5464f383a5c9b2a458b983555cc01
tzrocky <tzrocky@yahoo.fr>
2022-01-05 20:18:27 +0100

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -8,10 +8,13 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
+        """Check the sum of first and second params."""
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
+        """Check the substract of first and second params."""
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
+        """Check the product of first and second params."""
         assert 100 == calculator.multiply(10, 10)
