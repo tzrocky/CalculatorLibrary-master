a43cf6d7826491f7d951fe39df39545f725bf31d
tzrocky <tzrocky@yahoo.fr>
2022-01-05 19:59:38 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,12 +2,15 @@
 
 
 def add(first_term, second_term):
+    '''Takes in two numbers first_term and second_term, returns the sum of two'''
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
+    '''Takes in two numbers first_term and second_term, returns the substract of two'''
     return first_term - second_term
 
 
 def multiply(first_term, second_term):
+    '''Takes in two numbers first_term and second_term, returns the product of two'''
     return first_term * second_term
