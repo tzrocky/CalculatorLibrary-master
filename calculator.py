68b592b06d5a90717f4c4d4450b089651718ed25
tzrocky <tzrocky@yahoo.fr>
2022-01-05 20:14:44 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,15 +2,15 @@
 
 
 def add(first_term, second_term):
-    """Returns the sum of first and second params."""
+    '''Calculate the sum of first and second params.'''
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
-    """Returns the substract of first and second params."""
+    """Calculate the substract of first and second params."""
     return first_term - second_term
 
 
 def multiply(first_term, second_term):
-    """Returns the product of first and second params."""
+    """Calculate the product of first and second params."""
     return first_term * second_term
